ef8cad461fa62a55d6329c1475f662b42eb75461
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-24 02:37:45 +0530

    css for container

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import "./App.css";
 function App() {
   let foodItems = ["Dal", "Milk", "Vegetables"];
   return (
-    <Container>
-      <h1>Hello World</h1>
-      <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodItems items={foodItems}></FoodItems>
-    </Container>
+    <>
+      <Container>
+        <h1>Hello World</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      {/* <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          explicabo quisquam laborum facilis qui tempora reiciendis, optio
+          asperiores nesciunt saepe necessitatibus architecto vitae officiis
+          ipsa blanditiis dolores porro tenetur odio!
+        </p>
+      </Container> */}
+    </>
   );
 }
 
