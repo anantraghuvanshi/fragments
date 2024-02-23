e608a1b716f77a0f6ccef9e5300b797d2543625f
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-24 02:50:36 +0530

    using onClick

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,19 @@
-const Items = (props) => {
-  return <li className="list-group-item">{props.foodItem}</li>;
+const Items = ({ foodItem }) => {
+  const handleBuyButtonClicked = (event) => {
+    console.log(`${foodItem} is bought`);
+  };
+
+  return (
+    <li className="list-group-item">
+      <span>{foodItem}</span>
+      <button
+        className="btn btn-info btn-custom"
+        onClick={(event) => handleBuyButtonClicked(event)}
+      >
+        Buy
+      </button>
+    </li>
+  );
 };
 
 export default Items;
