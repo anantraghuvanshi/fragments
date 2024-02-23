1b1d8a8c06da47ef92b4668588757c2e3e8c44b9
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-24 02:31:54 +0530

    global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
+import "./App.css";
 
 function App() {
   let foodItems = ["Dal", "Milk", "Vegetables"];
   return (
-    <>
+    <Container>
       <h1>Hello World</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+    </Container>
   );
 }
 
